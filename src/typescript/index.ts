7bc406678aabf06bb515c10c5be49793f57cc0e3
Max-Andre <zilsmc@gmail.com>
2019-09-30 16:04:58 +0200

    added basic calculation

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,9 +1,17 @@
 // Validation
-
-
+const perform;
 // Calculation
-
+const pricePerDiaper = () => {
+  const description = document.getElementById('description').value;
+  const price = document.getElementById('price').value;
+  const quantity = document.getElementById('quantity').value;
+  return (
+    `The price per diaper for ${description} is R` + price / quantity + '.'
+  );
+};
 // Display on screen
-
+document.getElementById('btn').addEventListener('click', () => {
+  document.getElementById('result-list').innerHTML = pricePerDiaper();
+});
 
 // make comparison
